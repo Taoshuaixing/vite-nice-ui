8632314d14be9819323cb4d6ad555d86d9cf11e0
Taoshuaixing <1554307063@qq.com>
2023-07-11 16:26:56 +0800

    update

---
+++ dist/vite-nice-ui.js
@@ -0,0 +1,243 @@
+import { defineComponent, computed, openBlock, createElementBlock, normalizeClass, createElementVNode, withModifiers, normalizeStyle, withDirectives, vShow, renderSlot, createTextVNode, toDisplayString, useSlots, ref, createCommentVNode, createVNode, Transition, withCtx } from "vue";
+const _hoisted_1$1 = ["href", "target", "disabled"];
+const _hoisted_2 = { class: "u-spin-circle" };
+const _hoisted_3 = { class: "u-text" };
+const _sfc_main$1 = /* @__PURE__ */ defineComponent({
+  ...{
+    name: "nButton"
+  },
+  __name: "Button",
+  props: {
+    name: { default: "按钮" },
+    type: { default: "default" },
+    effect: { default: "fade" },
+    size: { default: "middle" },
+    width: { default: 0 },
+    height: { default: 0 },
+    borderRadius: { default: 5 },
+    route: { default: () => {
+      return {};
+    } },
+    target: { default: "_self" },
+    disabled: { type: Boolean, default: false },
+    loading: { type: Boolean, default: false },
+    center: { type: Boolean, default: false },
+    bgColor: { default: "" },
+    textColor: { default: "" }
+  },
+  setup(__props) {
+    const props = __props;
+    const isRoute = computed(() => {
+      if (JSON.stringify(props.route) === "{}") {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    function getUrl(route) {
+      var targetUrl = route.path;
+      if (route.query && JSON.stringify(route.query) !== "{}") {
+        const query = route.query;
+        Object.keys(query).forEach((param, index) => {
+          if (index === 0) {
+            targetUrl = targetUrl + "?" + param + "=" + query[param];
+          } else {
+            targetUrl = targetUrl + "&" + param + "=" + query[param];
+          }
+        });
+      }
+      return targetUrl;
+    }
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", {
+        class: normalizeClass(["n-btn-wrap", { "center": _ctx.center }])
+      }, [
+        createElementVNode("a", {
+          onClick: _cache[0] || (_cache[0] = withModifiers(($event) => isRoute.value ? () => false : _ctx.$emit("click", $event), ["stop"])),
+          href: getUrl(_ctx.route),
+          target: isRoute.value ? _ctx.target : "_self",
+          disabled: _ctx.disabled,
+          class: normalizeClass(["n-btn", [_ctx.type, _ctx.size, { [_ctx.effect]: _ctx.type === "default", widthType: _ctx.width, disabled: _ctx.disabled, "n-btn-loading": !isRoute.value && _ctx.loading }]]),
+          style: normalizeStyle(`border-radius: ${_ctx.borderRadius}px; width: ${_ctx.width ? _ctx.width + "px" : "auto"}; height: ${_ctx.height ? _ctx.height + "px" : "auto"}; line-height: ${_ctx.height - 2}px; background-color: ${_ctx.bgColor};color: ${_ctx.textColor};`)
+        }, [
+          withDirectives(createElementVNode("span", {
+            class: normalizeClass(["n-loading-icon", { "show-spin": _ctx.loading }])
+          }, [
+            withDirectives(createElementVNode("span", _hoisted_2, null, 512), [
+              [vShow, _ctx.loading]
+            ])
+          ], 2), [
+            [vShow, !isRoute.value]
+          ]),
+          createElementVNode("span", _hoisted_3, [
+            renderSlot(_ctx.$slots, "default", {}, () => [
+              createTextVNode(toDisplayString(_ctx.name), 1)
+            ], true)
+          ])
+        ], 14, _hoisted_1$1)
+      ], 2);
+    };
+  }
+});
+const Button_vue_vue_type_style_index_0_scoped_db9c1064_lang = "";
+const _export_sfc = (sfc, props) => {
+  const target = sfc.__vccOpts || sfc;
+  for (const [key, val] of props) {
+    target[key] = val;
+  }
+  return target;
+};
+const nButton = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["__scopeId", "data-v-db9c1064"]]);
+nButton.install = (app) => {
+  app.component(nButton.name, nButton);
+};
+const _hoisted_1 = ["type", "value", "disabled", "placeholder", "autofocus", "readonly", "form"];
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  ...{
+    name: "nInput"
+  },
+  __name: "Input",
+  props: {
+    modelValue: { default: "" },
+    disabled: { type: Boolean, default: false },
+    clearable: { type: Boolean, default: false },
+    showPassword: { type: Boolean, default: false },
+    type: { default: "input" },
+    size: { default: "default" },
+    leftIcon: { default: "" },
+    rightIcon: { default: "" },
+    placeholder: { default: "" },
+    autofocus: { type: Boolean, default: false },
+    focusColor: { default: "" },
+    readonly: { type: Boolean, default: false },
+    form: { default: "" }
+  },
+  emits: ["update:modelValue", "clear", "focus", "blur", "input", "change"],
+  setup(__props, { emit }) {
+    const props = __props;
+    const slot = useSlots();
+    const isStyle = ref({});
+    const inptype = ref(props.type);
+    let focusStyle = {
+      "width": !!slot.btn ? "auto" : "100%",
+      "float": !!slot.btn ? "left" : "auto",
+      "border-radius": !!slot.btn ? "4px 0 0 4px" : "4px",
+      "border-color": "#dcdfe6f6"
+    };
+    isStyle.value = focusStyle;
+    const focus = (e) => {
+      focusStyle["border-color"] = "#9708CC";
+      isStyle.value = {
+        "width": !!slot.btn ? "auto" : "100%",
+        "float": !!slot.btn ? "left" : "auto",
+        "border-radius": !!slot.btn ? "4px 0 0 4px" : "4px",
+        "border-color": props.focusColor
+      };
+      emit("focus", e);
+    };
+    const blur = (e) => {
+      isStyle.value = {
+        "width": !!slot.btn ? "auto" : "100%",
+        "float": !!slot.btn ? "left" : "auto",
+        "border-radius": !!slot.btn ? "4px 0 0 4px" : "4px"
+      };
+      emit("blur", e);
+    };
+    const iptChange = (e) => {
+      emit("update:modelValue", e.target.value);
+      emit("input", e.target.value);
+    };
+    const change = (e) => {
+      emit("change", e);
+    };
+    const clear = () => {
+      emit("update:modelValue", "");
+      emit("clear");
+    };
+    const showPwd = (e) => {
+      if (inptype.value == "text") {
+        inptype.value = "password";
+      } else {
+        inptype.value = "text";
+      }
+      console.log(e);
+    };
+    const isClass = computed(() => {
+      return [
+        props.clearable ? "nice-input-clearable" : props.size == "default" ? "nice-input-default" : `nice-input-${props.size}`,
+        props.leftIcon != "" ? `nice-input-left-icon-${props.size}` : !props.clearable ? props.rightIcon != "" ? `nice-input-right-icon-${props.size}` : "" : "",
+        props.disabled ? "nice-input-disabled" : "",
+        props.type == "password" ? props.showPassword ? `nice-input-password-showpassword-${props.size}` : `nice-input-password-${props.size}` : ""
+      ];
+    });
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", {
+        class: normalizeClass([`nice-group-input-${_ctx.size}`]),
+        style: { "min-width": "auto" }
+      }, [
+        createElementVNode("div", {
+          class: normalizeClass(isClass.value),
+          style: normalizeStyle([isStyle.value, {}])
+        }, [
+          !_ctx.showPassword && _ctx.leftIcon != "" ? (openBlock(), createElementBlock("i", {
+            key: 0,
+            class: normalizeClass(["left-icon", "iconfont", _ctx.leftIcon])
+          }, null, 2)) : createCommentVNode("", true),
+          createElementVNode("input", {
+            type: inptype.value,
+            onFocus: focus,
+            onBlur: blur,
+            value: _ctx.modelValue,
+            onInput: iptChange,
+            disabled: _ctx.disabled,
+            onChange: change,
+            placeholder: _ctx.placeholder,
+            autofocus: _ctx.autofocus,
+            readonly: _ctx.readonly,
+            form: _ctx.form
+          }, null, 40, _hoisted_1),
+          createVNode(Transition, { name: "slide-fade" }, {
+            default: withCtx(() => [
+              !_ctx.showPassword && _ctx.clearable && _ctx.modelValue != "" ? (openBlock(), createElementBlock("i", {
+                key: 0,
+                class: "clearable-icon iconfont m-icon-close",
+                onClick: clear
+              })) : createCommentVNode("", true)
+            ]),
+            _: 1
+          }),
+          !_ctx.showPassword && _ctx.rightIcon != "" ? (openBlock(), createElementBlock("i", {
+            key: 1,
+            class: normalizeClass(["right-icon", "iconfont", _ctx.rightIcon])
+          }, null, 2)) : createCommentVNode("", true),
+          _ctx.showPassword ? (openBlock(), createElementBlock("i", {
+            key: 2,
+            class: normalizeClass(["password-icon", "iconfont m-icon-browse"]),
+            onClick: _cache[0] || (_cache[0] = ($event) => showPwd(_ctx.type))
+          })) : createCommentVNode("", true)
+        ], 6),
+        renderSlot(_ctx.$slots, "btn", {}, void 0, true)
+      ], 2);
+    };
+  }
+});
+const Input_vue_vue_type_style_index_0_scoped_dbf09cfa_lang = "";
+const nInput = /* @__PURE__ */ _export_sfc(_sfc_main, [["__scopeId", "data-v-dbf09cfa"]]);
+nInput.install = (app) => {
+  app.component(nInput.name, nInput);
+};
+const components = [nButton, nInput];
+console.log("components:", components);
+const install = (app) => {
+  components.forEach((component) => {
+    app.component(component.name, component);
+  });
+};
+const ViteNiceUI = {
+  install
+};
+export {
+  ViteNiceUI as default,
+  nButton,
+  nInput
+};
---
+++ script/deploy.sh
@@ -0,0 +1,35 @@
+# /bin/bash
+###
+ # @Descripttion: 
+ # @version: 
+ # @Author: 陶帅星
+ # @Date: 2023-07-11 16:15:24
+ # @LastEditors: 陶帅星
+ # @LastEditTime: 2023-07-11 16:24:16
+### 
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 重新打包组件库
+npm run build
+
+# 打包生成静态文件
+npm run docs:build
+
+# 进入待发布的 dist/ 目录
+cd docs/.vitepress/dist
+
+# 提交打包静态网站到 github-pages 分支
+git init
+git add .
+git commit -m 'deploy'
+
+# 部署到 https://<username>.github.io/<repo>
+git push -f git@github.com:Taoshuaixing/vite-nice-ui.git main:github-pages
+
+# 提交所有代码到github
+cd ../../../
+git add .
+git commit -m 'update'
+git push
